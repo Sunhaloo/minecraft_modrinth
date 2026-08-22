618e5f1948773e6ac7962f82346b44a92a7f0760
Sunhaloo <azmaan151003@gmail.com>
2026-08-22 22:18:50 +0400

    A + U: Function to read data from a file --> Need to swap out `os` with `pathlib`

--- src/minecraft_modrinth/main.py
+++ src/minecraft_modrinth/main.py
@@ -1,9 +1,11 @@
+from os import getcwd
+
 from minecraft_modrinth.utils import input
 
 
 # our main function for the whole 'minecraft_modrinth' project
 def main() -> None:
-    print("Hello from minecraft-modrinth!")
+    # mods = input.read_mods_file("minecraft_mods.txt")
+    mods = input.read_mods_file(getcwd() + "/src/minecraft_modrinth/minecraft_mods.txt")
 
-    # INFO: testing
-    input.input_test()
+    print(f"Mods in file: {mods}")
--- src/minecraft_modrinth/utils/input.py
+++ src/minecraft_modrinth/utils/input.py
@@ -1,5 +1,26 @@
 # helper functions / utilities related to the input of data in the program
 
 
-def input_test():
-    print("Hello World From 'utils/input.py'!")
+# read the mods written in the text file and popuplate list
+def read_mods_file(file_path: str) -> list[str]:
+    mods_list: list[str] = []
+
+    try:
+        # read each Minecraft mods written inside the file and populate list
+        with open(file_path, "r") as mods_file:
+            for line in mods_file:
+                # remove all the unncessary "baggage" with the line
+                stripped_line = line.strip()
+
+                # check if the line contains Minecraft mods name or is a comment
+                if not stripped_line or stripped_line.startswith("#"):
+                    continue
+
+                else:
+                    # add the required Minecraft mods to the list
+                    mods_list.append(stripped_line.lower())
+
+    except FileNotFoundError as e:
+        print(e)
+
+    return mods_list
