2b75f1310d16db3e1078f8b3dfa350f6b69bf275
Sunhaloo <azmaan151003@gmail.com>
2026-08-22 15:12:38 +0400

    U: Setup project structure for coding

---
+++ src/minecraft_modrinth/main.py
@@ -0,0 +1,9 @@
+from minecraft_modrinth.utils import input
+
+
+# our main function for the whole 'minecraft_modrinth' project
+def main() -> None:
+    print("Hello from minecraft-modrinth!")
+
+    # INFO: testing
+    input.input_test()
---
+++ src/minecraft_modrinth/utils/input.py
@@ -0,0 +1,5 @@
+# helper functions / utilities related to the input of data in the program
+
+
+def input_test():
+    print("Hello World From 'utils/input.py'!")
