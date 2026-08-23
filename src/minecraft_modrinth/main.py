375b9a0d0f4e4bd13158712b36585ce04a9bb209
Sunhaloo <azmaan151003@gmail.com>
2026-08-23 10:45:24 +0400

    U: Swapped out path things from `os` with `Path` from `pathlib`

--- src/minecraft_modrinth/main.py
+++ src/minecraft_modrinth/main.py
@@ -1,11 +1,17 @@
-from os import getcwd
+from pathlib import Path
 
 from minecraft_modrinth.utils import input
+
+# variable to get the directory where we are running the `main.py` file
+MODULE_DIR = Path(__file__).resolve().parent
+
+# therefore create a variable for the `minecraft_mods` text file
+# INFO: need to convert 'Path' object into a string to be able to use it
+MODS_FILE = str(MODULE_DIR / "minecraft_mods.txt")
 
 
 # our main function for the whole 'minecraft_modrinth' project
 def main() -> None:
-    # mods = input.read_mods_file("minecraft_mods.txt")
-    mods = input.read_mods_file(getcwd() + "/src/minecraft_modrinth/minecraft_mods.txt")
+    mods = input.read_mods_file(MODS_FILE)
 
     print(f"Mods in file: {mods}")
