1a487b6f9a2523c6d401341194890efae4223326
Sunhaloo <azmaan151003@gmail.com>
2026-08-26 19:10:54 +0400

    U: We are now able to download mods found inside the `minecraft_mods.txt` file

--- src/minecraft_modrinth/main.py
+++ src/minecraft_modrinth/main.py
@@ -16,9 +16,16 @@ API_URL: str = "https://api.modrinth.com/v2"
 # our main function for the whole 'minecraft_modrinth' project
 def main() -> None:
     # create a file to get the data ==> basically following 'Downrinth'
-    # INFO: will try to change it so that we don't write to any file ==> much more simpler
     json_file = str(MODULE_DIR / "api_response.json")
 
+    # call the function to read the data from the `minecraft_modrinth.txt` file
+    mods_list: list[str] = input.read_mods_file(MODS_FILE)
+
+    # iterate through the list of mods
+    for i in range(len(mods_list)):
+        # download each of the mods found inside that list
+        api_things.get_cdn_url(API_URL, mods_list[i], "fabric", "1.21.1", json_file)
+
     # call the function to get the CDN URL
-    api_things.get_cdn_url(API_URL, "sodium", "fabric", "1.21.1", json_file)
-    api_things.get_cdn_url(API_URL, "entityculling", "fabric", "1.21.1", json_file)
+    # api_things.get_cdn_url(API_URL, "sodium", "fabric", "1.21.1", json_file)
+    # api_things.get_cdn_url(API_URL, "entityculling", "fabric", "1.21.1", json_file)
